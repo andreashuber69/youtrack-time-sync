93e9f466cb29b8fbc441f8773244152becdfbbc8
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-11-08 11:29:19 +0100

    Fix warnings

--- src/App.vue
+++ src/App.vue
@@ -23,17 +23,17 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   },
-  data () {
+  data() {
     return {
       //
-    }
-  }
-}
+    };
+  },
+};
 </script>
