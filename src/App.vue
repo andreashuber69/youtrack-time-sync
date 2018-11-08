29a4b128c5f8c1a7853c89ed842a07b55fb725c1
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-11-08 18:23:45 +0100

    Split into .vue and .vue.ts files

--- src/App.vue
+++ src/App.vue
@@ -20,35 +20,16 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
         <v-icon>open_in_new</v-icon>
       </v-btn>
     </v-toolbar>
-
     <v-content>
       <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld";
-
-// tslint:disable-next-line:no-default-export
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-  data() {
-    return {
-      //
-    };
-  },
-};
+<script src="./App.vue.ts" lang="ts">
 </script>
---
+++ src/components/HelloWorld.vue.ts
@@ -0,0 +1,67 @@
+// Copyright (C) 2018 Andreas Huber Dönni
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+
+// tslint:disable-next-line:no-default-export
+export default {
+    data: () => ({
+        ecosystem: [
+            {
+                text: "vuetify-loader",
+                href: "https://github.com/vuetifyjs/vuetify-loader",
+            },
+            {
+                text: "github",
+                href: "https://github.com/vuetifyjs/vuetify",
+            },
+            {
+                text: "awesome-vuetify",
+                href: "https://github.com/vuetifyjs/awesome-vuetify",
+            },
+        ],
+        importantLinks: [
+            {
+                text: "Documentation",
+                href: "https://vuetifyjs.com",
+            },
+            {
+                text: "Chat",
+                href: "https://community.vuetifyjs.com",
+            },
+            {
+                text: "Made with Vuetify",
+                href: "https://madewithvuetifyjs.com",
+            },
+            {
+                text: "Twitter",
+                href: "https://twitter.com/vuetifyjs",
+            },
+            {
+                text: "Articles",
+                href: "https://medium.com/vuetify",
+            },
+        ],
+        whatsNext: [
+            {
+                text: "Explore components",
+                href: "https://vuetifyjs.com/components/api-explorer",
+            },
+            {
+                text: "Select a layout",
+                href: "https://vuetifyjs.com/layout/pre-defined",
+            },
+            {
+                text: "Frequently Asked Questions",
+                href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+            },
+        ],
+    }),
+};
