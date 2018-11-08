ac75dcaf8fecd774e2becaa4e67e1ee6e58f2590
Andreas Huber <ahd6974-githubcom@outlook.com>
2018-11-08 13:28:24 +0100

    Add copyright notices and suppress tslint issues

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
-import Vue from 'vue';
-import './plugins/vuetify';
-import App from './App.vue';
+// tslint:disable:file-name-casing
+// Copyright (C) 2018 Andreas Huber Dönni
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+
+import Vue from "vue";
+// tslint:disable-next-line:match-default-export-name
+import App from "./App.vue";
+// tslint:disable-next-line:no-import-side-effect
+import "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,10 +1,23 @@
-import Vue, { VNode } from 'vue';
+// tslint:disable:file-name-casing
+// Copyright (C) 2018 Andreas Huber Dönni
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+
+import Vue, { VNode } from "vue";
 
 declare global {
   namespace JSX {
-    // tslint:disable no-empty-interface
+    // tslint:disable-next-line:no-empty-interface interface-name
     interface Element extends VNode {}
-    // tslint:disable no-empty-interface
+    // tslint:disable-next-line:no-empty-interface interface-name
     interface ElementClass extends Vue {}
     interface IntrinsicElements {
       [elem: string]: any;
