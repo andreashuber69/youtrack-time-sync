f8e22d19b62582e11b2c0331bedc36187d953b31
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-11-08 21:32:15 +0100

    Implement component as class

--- src/components/HelloWorld.vue.ts
+++ src/components/HelloWorld.vue.ts
@@ -10,58 +10,61 @@
 // You should have received a copy of the GNU General Public License along with this program. If not, see
 // <http://www.gnu.org/licenses/>.
 
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
 // tslint:disable-next-line:no-default-export
-export default {
-    data: () => ({
-        ecosystem: [
-            {
-                text: "vuetify-loader",
-                href: "https://github.com/vuetifyjs/vuetify-loader",
-            },
-            {
-                text: "github",
-                href: "https://github.com/vuetifyjs/vuetify",
-            },
-            {
-                text: "awesome-vuetify",
-                href: "https://github.com/vuetifyjs/awesome-vuetify",
-            },
-        ],
-        importantLinks: [
-            {
-                text: "Documentation",
-                href: "https://vuetifyjs.com",
-            },
-            {
-                text: "Chat",
-                href: "https://community.vuetifyjs.com",
-            },
-            {
-                text: "Made with Vuetify",
-                href: "https://madewithvuetifyjs.com",
-            },
-            {
-                text: "Twitter",
-                href: "https://twitter.com/vuetifyjs",
-            },
-            {
-                text: "Articles",
-                href: "https://medium.com/vuetify",
-            },
-        ],
-        whatsNext: [
-            {
-                text: "Explore components",
-                href: "https://vuetifyjs.com/components/api-explorer",
-            },
-            {
-                text: "Select a layout",
-                href: "https://vuetifyjs.com/layout/pre-defined",
-            },
-            {
-                text: "Frequently Asked Questions",
-                href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-            },
-        ],
-    }),
-};
+export default class HelloWorld extends Vue {
+    public ecosystem = [
+        {
+            text: "vuetify-loader",
+            href: "https://github.com/vuetifyjs/vuetify-loader",
+        },
+        {
+            text: "github",
+            href: "https://github.com/vuetifyjs/vuetify",
+        },
+        {
+            text: "awesome-vuetify",
+            href: "https://github.com/vuetifyjs/awesome-vuetify",
+        },
+    ];
+
+    public importantLinks = [
+        {
+            text: "Documentation",
+            href: "https://vuetifyjs.com",
+        },
+        {
+            text: "Chat",
+            href: "https://community.vuetifyjs.com",
+        },
+        {
+            text: "Made with Vuetify",
+            href: "https://madewithvuetifyjs.com",
+        },
+        {
+            text: "Twitter",
+            href: "https://twitter.com/vuetifyjs",
+        },
+        {
+            text: "Articles",
+            href: "https://medium.com/vuetify",
+        },
+    ];
+
+    public whatsNext = [
+        {
+            text: "Explore components",
+            href: "https://vuetifyjs.com/components/api-explorer",
+        },
+        {
+            text: "Select a layout",
+            href: "https://vuetifyjs.com/layout/pre-defined",
+        },
+        {
+            text: "Frequently Asked Questions",
+            href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+        },
+    ];
+}
