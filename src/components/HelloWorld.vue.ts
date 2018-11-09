edc1e99302d10b955f4f3aabeaf035dbe2951d73
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-11-09 22:27:08 +0100

    Remove vuetify content

--- src/components/HelloWorld.vue.ts
+++ src/components/HelloWorld.vue.ts
@@ -10,61 +10,25 @@
 // You should have received a copy of the GNU General Public License along with this program. If not, see
 // <http://www.gnu.org/licenses/>.
 
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Model } from "../model/Model";
 
 @Component
 // tslint:disable-next-line:no-default-export
 export default class HelloWorld extends Vue {
-    public ecosystem = [
-        {
-            text: "vuetify-loader",
-            href: "https://github.com/vuetifyjs/vuetify-loader",
-        },
-        {
-            text: "github",
-            href: "https://github.com/vuetifyjs/vuetify",
-        },
-        {
-            text: "awesome-vuetify",
-            href: "https://github.com/vuetifyjs/awesome-vuetify",
-        },
-    ];
+    @Prop()
+    public model?: Model;
 
-    public importantLinks = [
-        {
-            text: "Documentation",
-            href: "https://vuetifyjs.com",
-        },
-        {
-            text: "Chat",
-            href: "https://community.vuetifyjs.com",
-        },
-        {
-            text: "Made with Vuetify",
-            href: "https://madewithvuetifyjs.com",
-        },
-        {
-            text: "Twitter",
-            href: "https://twitter.com/vuetifyjs",
-        },
-        {
-            text: "Articles",
-            href: "https://medium.com/vuetify",
-        },
-    ];
+    public onSubmitClicked(event: MouseEvent) {
+        if (this.isValid()) {
+            // Whatever
+        }
+    }
 
-    public whatsNext = [
-        {
-            text: "Explore components",
-            href: "https://vuetifyjs.com/components/api-explorer",
-        },
-        {
-            text: "Select a layout",
-            href: "https://vuetifyjs.com/layout/pre-defined",
-        },
-        {
-            text: "Frequently Asked Questions",
-            href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-        },
-    ];
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private isValid() {
+        // tslint:disable-next-line:no-unsafe-any
+        return (this.$refs.form as any).validate();
+    }
 }
