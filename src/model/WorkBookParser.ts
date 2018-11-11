85bc34d97c4b8a19377923c82f1860357bae215f
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2018-11-11 19:37:01 +0100

    Expand sheet parsing

--- src/model/WorkBookParser.ts
+++ src/model/WorkBookParser.ts
@@ -13,10 +13,24 @@
 import { WorkBook, WorkSheet } from "xlsx";
 
 export interface ISpentTime {
+    readonly paidAbsence: boolean;
     readonly date: Date;
     readonly title: string;
     readonly type: string;
     readonly comment: string;
+    readonly durationDays: number;
+}
+
+interface IRow {
+    readonly errorPrefix: string;
+    readonly row: number;
+    readonly holidays?: ICell;
+    readonly otherPaidAbsences?: ICell;
+    readonly start?: ICell;
+    readonly end?: ICell;
+    readonly title?: ICell;
+    readonly type?: ICell;
+    readonly comment?: ICell;
 }
 
 interface ICell {
@@ -58,49 +72,70 @@ export class WorkBookParser {
         this.checkRange((left !== "A") || (right !== "G") || (top !== 1) || (bottom < firstDataRow), sheetName, range);
 
         for (let row = firstDataRow; row <= bottom; ++row) {
-            WorkBookParser.parseRow(sheet, row, sheetName);
+            WorkBookParser.parseRow({
+                errorPrefix: `In sheet ${sheetName}, `,
+                // tslint:disable-next-line:object-literal-key-quotes
+                "row": row, // TODO
+                holidays: sheet[`A${row}`] as ICell | undefined,
+                otherPaidAbsences: sheet[`B${row}`] as ICell | undefined,
+                start: sheet[`C${row}`] as ICell | undefined,
+                end: sheet[`D${row}`] as ICell | undefined,
+                title: sheet[`E${row}`] as ICell | undefined,
+                type: sheet[`F${row}`] as ICell | undefined,
+                comment: sheet[`G${row}`] as ICell | undefined,
+            });
         }
     }
 
-    private static parseRow(sheet: WorkSheet, row: number, sheetName: string) {
-        const holidays = sheet[`A${row}`] as ICell | undefined;
-        const otherPaidAbsences = sheet[`B${row}`] as ICell | undefined;
-        const start = sheet[`C${row}`] as ICell | undefined;
-        const end = sheet[`D${row}`] as ICell | undefined;
-        const title = sheet[`E${row}`] as ICell | undefined;
-        const type = sheet[`F${row}`] as ICell | undefined;
-        const comment = sheet[`G${row}`] as ICell | undefined;
-
-        if (holidays || otherPaidAbsences) {
-            if (!start || !end || (start.f !== undefined) || (end.f === undefined)) {
-                throw new Error(`In sheet ${sheetName}, C${row} must be fixed and D${row} must be floating.`);
-            }
-        } else if (start && end) {
-            if ((start.f === undefined) !== (end.f === undefined)) {
+    private static parseRow(row: IRow) {
+        if (row.holidays || row.otherPaidAbsences) {
+            if (!row.start || !row.end || (row.start.f !== undefined) || (row.end.f === undefined)) {
                 throw new Error(
-                    `In sheet ${sheetName}, C${row} and D${row} must either be both fixed or both floating.`);
+                    `${row.errorPrefix}C${row.row} must be fixed and D${row.row} must be floating.`);
             }
 
-            if ((typeof start.v !== "number") || (typeof end.v !== "number")) {
-                throw new Error(`In sheet ${sheetName}, C${row} and D${row} must both be dates.`);
+            // TODO: Process paid absence
+        } else {
+            WorkBookParser.parsePresence(row);
+        }
+    }
+
+    private static parsePresence(row: IRow) {
+        if (!row.start !== !row.end) {
+            throw new Error(
+                `${row.errorPrefix}C${row.row} and D${row.row} must either be both empty or non-empty.`);
+        }
+
+        if (!row.start || !row.end) {
+            return;
+        }
+
+        if ((row.start.f === undefined) !== (row.end.f === undefined)) {
+            throw new Error(
+                `${row.errorPrefix}C${row.row} and D${row.row} must either be both values or both formulas.`);
+        }
+
+        if ((typeof row.start.v !== "number") || (typeof row.end.v !== "number")) {
+            throw new Error(`${row.errorPrefix}C${row.row} and D${row.row} must both be dates.`);
+        }
+
+        if (row.start.f === undefined) {
+            const time = row.end.v - row.start.v;
+
+            if (time < 0) {
+                throw new Error(`${row.errorPrefix}C${row.row} must be smaller than D${row.row}.`);
             }
 
-            if (start.f === undefined) {
-                const time = end.v - start.v;
-
-                if (time < 0) {
-                    throw new Error(`In sheet ${sheetName}, C${row} must be smaller than D${row}.`);
-                }
-
-                if (time >= 1) {
-                    throw new Error(`In sheet ${sheetName}, on row ${row} the spent time must be smaller than 1 day.`);
-                }
-
-                if (!title || !title.v) {
-                    throw new Error(`In sheet ${sheetName}, E${row} must not be empty.`);
-                }
-
+            if (time >= 1) {
+                throw new Error(
+                    `${row.errorPrefix}on row ${row.row} the spent time must be smaller than 1 day.`);
             }
+
+            if (!row.title || !row.title.v) {
+                throw new Error(`${row.errorPrefix}E${row.row} must not be empty.`);
+            }
+
+            // TODO: Process presence
         }
     }
 
