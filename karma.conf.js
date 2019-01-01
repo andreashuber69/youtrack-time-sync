b25d5b8d6c98fa098a2b37ea6c372370ae931051
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-01-01 20:17:26 +0100

    Make testing work

--- karma.conf.js
+++ karma.conf.js
@@ -2,15 +2,28 @@ module.exports = function(config) {
     config.set({
         frameworks: [ "jasmine", "karma-typescript" ],
         files: [
-            { pattern: "src/**/*.spec.ts" }
+            { pattern: "src/model/**/*.ts" }
         ],
         preprocessors: {
             "**/*.ts": [ "karma-typescript" ]
         },
         reporters: [ "dots", "karma-typescript" ],
         browsers: [ "Chrome" ],
+
+        // Set this to false while debugging
         singleRun: true,
+
         karmaTypescriptConfig: {
+            bundlerOptions: {
+                sourceMap: true
+            },
+            compilerOptions: {
+                module: "commonjs"
+            },
+            coverageOptions: {
+                // Set this to false while debugging
+                instrumentation: true
+            },
             tsconfig: "./tsconfig.json"
         }
     });
--- src/model/WorkBookParser.spec.ts
+++ src/model/WorkBookParser.spec.ts
@@ -1,7 +1,10 @@
+import { WorkBook } from "xlsx";
 import { WorkBookParser } from "./WorkBookParser";
 
 describe("WorkBookParser", () => {
     it("should parse", () => {
+        const workBook = undefined as any as WorkBook;
+        const whatever = WorkBookParser.parse(workBook);
         expect(true).toEqual(true);
     });
 });
