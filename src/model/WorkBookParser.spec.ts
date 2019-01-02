9942855dcfc58a8af9a3d631acd6a35248b92516
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-01-02 11:27:11 +0100

    Add first real test

--- src/model/WorkBookParser.spec.ts
+++ src/model/WorkBookParser.spec.ts
@@ -1,10 +1,35 @@
-import { WorkBook } from "xlsx";
+import { read } from "xlsx";
 import { WorkBookParser } from "./WorkBookParser";
 
-describe("WorkBookParser", () => {
-    it("should parse", () => {
-        const workBook = undefined as any as WorkBook;
-        const whatever = WorkBookParser.parse(workBook);
-        expect(true).toEqual(true);
+const readBlob = (blob: Blob) =>
+    new Promise<ArrayBuffer>((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (ev) => resolve(reader.result as ArrayBuffer);
+        reader.onerror = (ev) => reject("Unable to read file.");
+        reader.readAsArrayBuffer(blob);
+    });
+
+const loadTestSheet = async (name: string) => {
+    const url = `https://raw.githubusercontent.com/andreashuber69/youtrack-time-sync/master/src/model/test/${name}`;
+    let response: Response;
+
+    try {
+        response = await window.fetch(url);
+    } catch (e) {
+        throw new Error(`Network Error: ${e}`);
+    }
+
+    if (!response.ok) {
+        throw new Error(`Response Status: ${response.status} ${response.statusText}`);
+    }
+
+    return read(new Uint8Array(await readBlob(await response.blob())), { type: "array" });
+};
+
+describe("WorkBookParser.parse", () => {
+    it("should parse empty workbook", async () => {
+        const workBook = await loadTestSheet("Empty.xlsm");
+        const times = [ ...WorkBookParser.parse(workBook) ];
+        expect(times).toEqual([]);
     });
 });
