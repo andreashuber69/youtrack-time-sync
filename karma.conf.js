1795543f1e679cc7d3fb5a910b2ffb2b5ad89daf
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-01-06 16:45:07 +0100

    Fix CC issue

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,30 @@
+const karmaTypescriptConfig = {
+    bundlerOptions: {
+        entrypoints: /\.spec\.ts$/,
+        sourceMap: true
+    },
+    compilerDelay: 3000,
+    compilerOptions: {
+        module: "commonjs"
+    },
+    coverageOptions: {
+        // Set this to false while debugging
+        instrumentation: true
+    },
+    reports: {
+        lcovonly: {
+            directory: "coverage",
+            subdirectory: ".",
+            filename: "lcov.lcov"
+        },
+        html: {
+            directory: "coverage",
+            subdirectory: "."
+        }
+    },
+    tsconfig: "./tsconfig.json"
+};
+
 module.exports = function(config) {
     config.set({
         frameworks: [ "jasmine", "karma-typescript" ],
@@ -18,31 +45,6 @@ module.exports = function(config) {
         browsers: [ "Chrome", "ChromeHeadless" ],
         // Set this to false while debugging
         singleRun: true,
-        karmaTypescriptConfig: {
-            bundlerOptions: {
-                entrypoints: /\.spec\.ts$/,
-                sourceMap: true
-            },
-            compilerDelay: 3000,
-            compilerOptions: {
-                module: "commonjs"
-            },
-            coverageOptions: {
-                // Set this to false while debugging
-                instrumentation: true
-            },
-            reports: {
-                lcovonly: {
-                    directory: "coverage",
-                    subdirectory: ".",
-                    filename: "lcov.lcov"
-                },
-                html: {
-                    directory: "coverage",
-                    subdirectory: "."
-                }
-            },
-            tsconfig: "./tsconfig.json"
-        }
+        karmaTypescriptConfig: karmaTypescriptConfig
     });
 };
