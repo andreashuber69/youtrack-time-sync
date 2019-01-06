36fefceaa67d06e2eaa74c4bafb87033b12fc27f
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-01-06 16:38:16 +0100

    Fix CC issue

---
+++ src/model/BlobUtility.ts
@@ -0,0 +1,22 @@
+// Copyright (C) 2018 Andreas Huber Dönni
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+
+export class BlobUtility {
+    public static toArrayBuffer(blob: Blob) {
+        return new Promise<ArrayBuffer>((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result as ArrayBuffer);
+            reader.onerror = () => reject("Unable to read blob.");
+            reader.readAsArrayBuffer(blob);
+        });
+    }
+}
