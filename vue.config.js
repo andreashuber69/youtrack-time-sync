f2b3b0ac8843d24cf23af9e6ce10605b7a6dab88
Andreas Huber <ahd6974-githubcom@outlook.com>
2019-01-27 10:39:28 +0100

    Observe max line length

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,8 @@
 // <http://www.gnu.org/licenses/>.
 
 module.exports = {
-    // publicPath needs to be set differently depending on whether we build for actual deployment (e.g. on github gh-pages)
-    // or for local production mode testing. The following uses an exported environment variable to do just that.
-    // Obviously, this needs to bet set accordingly before building for deployment. 
+    // publicPath needs to be set differently depending on whether we build for actual deployment (e.g. on github
+    // gh-pages) or for local production mode testing. The following uses an exported environment variable to do just
+    // that. Obviously, this needs to bet set accordingly before building for deployment. 
     publicPath: process.env.WEBPACK_BASE_URL ? process.env.WEBPACK_BASE_URL : "/",
 }
